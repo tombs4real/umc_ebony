c3603f92851dceabd38e051303bf389648e2a645
James Tombs <james.tombs@utah.edu>
2018-08-24 15:53:49 -0600

    Initial build

---
+++ includes/footer.php
@@ -0,0 +1,128 @@
+<footer class="uu-footer">
+  <!-- footer top -->
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 text-center order-md-2">
+          <!-- department logo -->
+          <img src="img/blocku.svg" alt="" class="uu-block-logo">
+          <!-- end department logo -->
+          <!-- department name & address -->
+          <h5 class="department-name">THE UNIVERSITY OF UTAH</h5>
+          <p class="department-address">
+            201 PRESIDENTS CIRCLE<br />
+            SALT LAKE CITY, UT 84112<br />
+            801-581-7200
+          </p>
+          <!-- end department name & address -->
+        </div>
+        <div class="col-md-4 text-center text-md-left order-md-1">
+          <!-- menu block -->
+          <!-- menu block header -->
+          <h3 class="uu-main-nav-heading">Quick Resources</h3>
+          <hr>
+          <!-- end menu block header -->
+          <ul>
+            <li>
+              <a href="#">CIS Login</a>
+            </li>
+            <li>
+              <a href="#">U Mail</a>
+            </li>
+            <li>
+              <a href="#">Academic Advising Center</a>
+            </li>
+            <li>
+              <a href="#">@ The U</a>
+            </li>
+          </ul>
+          <!-- end menu block -->
+        </div>
+        <div class="col-md-4 text-center text-md-right order-md-3">
+          <!-- menu block -->
+          <!-- menu block header -->
+          <h3 class="uu-main-nav-heading">Quick Resources</h3>
+          <hr class="right">
+          <!-- end menu block header -->
+          <ul>
+            <li>
+              <a href="#">CIS Login</a>
+            </li>
+            <li>
+              <a href="#">U Mail</a>
+            </li>
+            <li>
+              <a href="#">Academic Advising Center</a>
+            </li>
+            <li>
+              <a href="#">@ The U</a>
+            </li>
+          </ul>
+          <!-- end menu block -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end footer top -->
+  <!-- footer bottom -->
+  <div class="footer-bottom">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-3 text-center">
+          <!-- site logo -->
+          <a href="#">
+            <img src="img/imagine-u-logo.png" alt="" class="uu-site-logo img-fluid margin-auto">
+          </a>
+          <!-- end site logo -->
+        </div>
+        <div class="col-md-6 text-center">
+          <div class="legal">
+            © 2018 The University of Utah
+            <ul>
+              <li>
+                <a href="#">Nondiscrimination & Accessibility</a>
+              </li>
+              <li>
+                <a href="#">Disclaimer</a>
+              </li>
+              <li>
+                <a href="#">Privacy</a>
+              </li>
+              <li>
+                <a href="#">Media Contacts</a>
+              </li>
+              <li>
+                <a href="#">Credits & Attributions</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 text-center">
+          <ul class="uu-social-list">
+            <li>
+              <a href="#">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-youtube-play" aria-hidden="true"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end footer bottom -->
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<header class="uu-header fixed">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <!-- u-logo -->
+        <a href="https://www.utah.edu/" target="_blank">
+          <img src="img/universityofutah.svg" alt="" class="uu-logo img-fluid margin-auto d-none d-lg-inline-flex">
+          <img src="img/blocku.svg" alt="" class="uu-block-logo d-inline-flex d-lg-none img-fluid margin-auto">
+        </a>
+        <!-- end u-logo -->
+        <!-- site title -->
+        <h1 class="uu-site-title d-none d-md-inline-flex d-lg-inline-flex">
+          <a href="#">
+            <span>UMC</span> University Marketing and Communications
+          </a>
+        </h1>
+        <!-- end site title -->
+        <button id="uu-search-trigger" class="uu-search-btn">
+          <i class="fa fa-search" aria-hidden="true"></i>
+        </button>
+        <!-- search button -->
+        <!-- end search button -->
+        <!-- menu button -->
+        <button id="uu-pushmenu-trigger" class="uu-hamburger uu-hamburger--x">
+          <span>MENU</span>
+        </button>
+        <!-- end menu button -->
+      </div>
+    </div>
+  </div>
+</header>
